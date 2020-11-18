69e55c75a7c40bd633e5e9cbbf5c25c209b37636
mhgitrepo <mehedi1921@gmail.com>
2020-11-18 23:37:24 +0600

    Double equal vs Tripple equal

---
+++ double-vs-tripple-equal.js
@@ -0,0 +1,74 @@
+/* Double Equal == */
+
+// const first = 5;
+// const second = 10;
+// if(first == second){
+//     console.log("Condition is true.");
+// } else{
+//     console.log("Condition is false.");
+// }
+//result => false;
+
+// const first = 5;
+// const second = 5;
+// if(first == second){
+//     console.log("Condition is true.");
+// } else{
+//     console.log("Condition is false.");
+// }
+//result => true;
+
+// const first = 5;
+// const second = "5";
+// if(first == second){
+//     console.log("Condition is true.");
+// } else{
+//     console.log("Condition is false.");
+// }
+//result => true; => data types won't be checked. But if the value is same, then the result will be true. 
+
+// const first = 1;
+// const second = true;
+// if(first == second){
+//     console.log("Condition is true.");
+// } else{
+//     console.log("Condition is false.");
+// }
+//by default 1 = true
+
+// const first = 1;
+// const second = false;
+// if(first == second){
+//     console.log("Condition is true.");
+// } else{
+//     console.log("Condition is false.");
+// }
+//by default 1 = false, that's why result is false.
+
+// const first = 0;
+// const second = false;
+// if(first == second){
+//     console.log("Condition is true.");
+// } else{
+//     console.log("Condition is false.");
+// }
+//by default 0 = false, that's why result is true;
+
+/* Triple Equal === */
+// const first = 5;
+// const second = "5";
+// if(first === second){
+//     console.log("Condition is true.");
+// } else{
+//     console.log("Condition is false.");
+// }
+//result => flase; => data types will be checked and value too. {stict}
+
+const first = 1;
+const second = true;
+if(first === second){
+    console.log("Condition is true.");
+} else{
+    console.log("Condition is false.");
+}
+// types are not same
