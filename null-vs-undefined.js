a3c5967402637ff0ed667cc0afa303bac6115b17
mhgitrepo <mehedi1921@gmail.com>
2020-11-18 23:13:46 +0600

    null vs undefined

--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -1,31 +1,31 @@
 /*unefined*/
 // undefined = false;
 
-// let name;
-// function add(num1, num2){
-//     console.log(num1 + num2);
-//     return;
-// }
+let name;
+function add(num1, num2){
+    console.log(num1 + num2);
+    return;
+}
 
-// function add(num1, num2/*num=10*/){
-//     // console.log(num2);
-//     console.log(num1, num2);
-// }
+function add(num1, num2/*num=10*/){
+    // console.log(num2);
+    console.log(num1, num2);
+}
 
-// const student = {name: "John", id: 54215, country: "USA"};
-// console.log(student.class);
+const student = {name: "John", id: 54215, country: "USA"};
+console.log(student.class);
 
-// let fun = undefined;
-// console.log(fun);
+let fun = undefined;
+console.log(fun);
 
-// let ages = [2, 5, 9];
-// console.log(ages[11]);//undefined.
+let ages = [2, 5, 9];
+console.log(ages[11]);//undefined.
 
 /*Null*/
 //not existent
 //you will explicitly set the null value;
-// let ages = [2, 5, 9];
-// console.log(ages[11]);//undefined.
+let ages = [2, 5, 9];
+console.log(ages[11]);//undefined.
 
 
 
