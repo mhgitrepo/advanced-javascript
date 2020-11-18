b16aa78f269a12e959e512bf2a432f100fb40f63
mhgitrepo <mehedi1921@gmail.com>
2020-11-18 22:26:25 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,43 @@
+//truthy
+// [] = true; //array
+// {} = true; //object
+// 1 = true; //number
+// " " = true;
+// "1" = true//sting
+// true = true;
+
+//falsy
+// 0 = false;// 0 is a negative value, that's why it's a falsy;
+// -0 = false;
+// 0n/1n/2n/.....9n = false;
+// "" = false;//empty string
+// null = false;
+// undefined = false;
+// NaN = false;
+// false = false;
+
+//truthy
+// var name = [];
+// var name = {};
+// var name = 1;
+// var name = " ";
+// var name = "10";
+// var name = true;
+
+//falsy
+// var name = 0;
+// var name = -0;
+// var name = 545n;
+// var name = "";
+// var name = null;
+// var name = undefined;
+// var name = NaN;
+// var name = false;
+
+//random
+var name = 540
+if(name >= 540){
+    console.log("Condition is true.");
+} else{
+    console.log("Condition is false.");
+}
