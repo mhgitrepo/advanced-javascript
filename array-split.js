638f12b02ac2f5dcfb350b136048eec6b7deb4ae
mhgitrepo <mehedi1921@gmail.com>
2020-11-19 16:28:20 +0600

    Array slice, split and join

---
+++ array-split.js
@@ -0,0 +1,26 @@
+//Array Slice
+/*
+const nums = [1, 2, 3, 4, 5, 6, 7, 8];
+const part = nums.slice(3, 6); //start after(index) , end before(index)
+
+console.log(part);
+console.log(nums);//original array won't be changed.
+*/
+
+//Array Splice
+/*
+const nums = [1, 2, 3, 4, 5, 6, 7, 8];
+// const removed = nums.splice(4, 2); //start after(index), delete count(indexing starts from 'start after' count)
+//items injecting
+const removed = nums.splice(4, 2, 80, 90, 95);
+console.log(removed);
+console.log(nums);
+*/
+
+
+//Array Join
+
+const nums = [1, 2, 3, 4, 5, 6, 7, 8];
+const together = nums.join(", ");
+
+console.log(together);
